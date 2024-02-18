682bfb126f9ddfbdf87ec00df28b7c14a5e2ac6e
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-02-18 19:35:39 +0500

    Site Ready

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Header from "@/Components/Header/Header"
+import Landing from "@/Components/Pages/Home/Landing"
+
+
+const page = () => {
+  return (
+    <>
+     <Header/>
+    <Landing/>
+    </>
+   
+  )
+}
+
+export default page
