9808ef040c1304de4f102fa74a37f4cee63f2839
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-02-23 01:54:08 -0800

    Update APrices.jsx

--- src/Components/Pages/About/APrices.jsx
+++ src/Components/Pages/About/APrices.jsx
@@ -1,11 +1,6 @@
 import { GiWaterDrop } from "react-icons/gi";
 
-
 const APrices = () => {
-
-
-
-
   return (
     <div className="p-30">
       <div className="max-w-screen-xl px-10 m-auto">
@@ -15,11 +10,14 @@ const APrices = () => {
         </div>
 
         <div className="grid grid-cols-3 gap-7 py-16 max-[1024px]:grid-cols-2 max-[670px]:grid-cols-1 max-[370px]:py-12">
-
           <article className=" bg-[#161616] text-center max-[370px]:p-7 items-center p-12 flex flex-col gap-5 shadow-lg transition-all	hover:shadow-[#009e66] ">
-            <i className="text-5xl text-[#009e66]"><GiWaterDrop /></i>
+            <i className="text-5xl text-[#009e66]">
+              <GiWaterDrop />
+            </i>
             <h3 className="heading4">Standard</h3>
-            <h4 className="heading5 tracking-widest	text-[#D4D4D4]	">$19/month</h4>
+            <h4 className="heading5 tracking-widest	text-[#D4D4D4]	">
+              $19/month
+            </h4>
             <div className="flex flex-col gap-5">
               <p className="paragraph">Mobile App Design</p>
               <p className="paragraph">Responsive Design</p>
@@ -27,12 +25,18 @@ const APrices = () => {
               <p className="paragraph">Web Design</p>
               <p className="paragraph">24/7 Support</p>
             </div>
-            <button className="rounded-full transition-all  hover:bg-[#009e67b9] px-7 py-2 bg-[#009e66]">Get Started</button>
+            <button className="rounded-full transition-all  hover:bg-[#009e67b9] px-7 py-2 bg-[#009e66]">
+              Get Started
+            </button>
           </article>
           <article className=" bg-[#161616] text-center items-center p-12 flex flex-col gap-5 shadow-lg transition-all	hover:shadow-[#009e66] ">
-            <i className="text-5xl text-[#009e66]"><GiWaterDrop /></i>
+            <i className="text-5xl text-[#009e66]">
+              <GiWaterDrop />
+            </i>
             <h3 className="heading4">Professional</h3>
-            <h4 className="heading5 tracking-widest	text-[#D4D4D4]	">$29/month</h4>
+            <h4 className="heading5 tracking-widest	text-[#D4D4D4]	">
+              $29/month
+            </h4>
             <div className="flex flex-col gap-5">
               <p className="paragraph">Mobile App Design</p>
               <p className="paragraph">Responsive Design</p>
@@ -40,12 +44,18 @@ const APrices = () => {
               <p className="paragraph">Web Design</p>
               <p className="paragraph">24/7 Support</p>
             </div>
-            <button className="rounded-full transition-all  hover:bg-[#009e67b9] px-7 py-2 bg-[#009e66]">Get Started</button>
+            <button className="rounded-full transition-all  hover:bg-[#009e67b9] px-7 py-2 bg-[#009e66]">
+              Get Started
+            </button>
           </article>
           <article className=" bg-[#161616] text-center items-center p-12 flex flex-col gap-5 shadow-lg transition-all	hover:shadow-[#009e66] ">
-            <i className="text-5xl text-[#009e66]"><GiWaterDrop /></i>
+            <i className="text-5xl text-[#009e66]">
+              <GiWaterDrop />
+            </i>
             <h3 className="heading4">Business</h3>
-            <h4 className="heading5 tracking-widest	text-[#D4D4D4]	">$39/month</h4>
+            <h4 className="heading5 tracking-widest	text-[#D4D4D4]	">
+              $39/month
+            </h4>
             <div className="flex flex-col gap-5">
               <p className="paragraph">Mobile App Design</p>
               <p className="paragraph">Responsive Design</p>
@@ -53,13 +63,14 @@ const APrices = () => {
               <p className="paragraph">Web Design</p>
               <p className="paragraph">24/7 Support</p>
             </div>
-            <button className="rounded-full transition-all  hover:bg-[#009e67b9] px-7 py-2 bg-[#009e66]">Get Started</button>
+            <button className="rounded-full transition-all  hover:bg-[#009e67b9] px-7 py-2 bg-[#009e66]">
+              Get Started
+            </button>
           </article>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default APrices
+export default APrices;
