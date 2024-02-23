e529933bb1182afaf19e281e1e9f2ef127a3d25a
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-02-23 01:59:44 -0800

    Update ProgressBar.jsx

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,53 +1,51 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 
 const ProgressBar = () => {
-    const [scrollPercentage, setScrollPercentage] = useState(0);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const windowHeight = window.innerHeight;
-            const documentHeight = document.documentElement.scrollHeight;
-            const scrollY = window.scrollY;
-            const scrollPercent = (scrollY / (documentHeight - windowHeight)) * 100;
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollY = window.scrollY;
+      const scrollPercent = (scrollY / (documentHeight - windowHeight)) * 100;
 
-            setScrollPercentage(scrollPercent);
-        };
+      setScrollPercentage(scrollPercent);
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    }, []);
+  const progressWidth = scrollPercentage;
 
-    const progressWidth = scrollPercentage;
-
-    return (
-        <div
-            id="progress-container"
-            style={{
-                height: "2px",
-                width: "100%",
-                backgroundColor: "#292929",
-                position: "fixed",
-                top: "0.1px",
-                zIndex: "9999999",
-                boxShadow: "0px 0px 40px 0px var(--color-primary)"
-            }}
-        >
-            <div
-                className="progress-fill"
-                style={{
-                    height: "100%",
-                    background: "#009e66",
-                    width: `${progressWidth}%`
-                }}
-            ></div>
-        </div>
-
-    );
+  return (
+    <div
+      id="progress-container"
+      style={{
+        height: "2px",
+        width: "100%",
+        backgroundColor: "#292929",
+        position: "fixed",
+        top: "0.1px",
+        zIndex: "9999999",
+        boxShadow: "0px 0px 40px 0px var(--color-primary)",
+      }}
+    >
+      <div
+        className="progress-fill"
+        style={{
+          height: "100%",
+          background: "#009e66",
+          width: `${progressWidth}%`,
+        }}
+      ></div>
+    </div>
+  );
 };
 
 export default ProgressBar;
