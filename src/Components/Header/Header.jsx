78c2718d6948ade3f98504d68dd209fd01d161eb
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-02-28 05:47:57 -0800

    Ready Complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,12 @@
 "use client";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Logo from "../../../public/Images/Logo.webp";
@@ -75,10 +83,48 @@ const Header = () => {
             <div className="hidden max-[650px]:block">
               <i className="text-4xl hover:text-[#009e66]">
                 {" "}
-                <HiOutlineMenuAlt3 />
+                <Sheet>
+                  <SheetTrigger>
+                    {" "}
+                    <HiOutlineMenuAlt3 />
+                  </SheetTrigger>
+                  <SheetContent className="bg-[#161616]">
+                    <SheetHeader>
+                      <SheetTitle className="text-white">
+                        <div>
+                          <Link href="/">
+                            <Image src={Logo} alt="Logo" width={110} />
+                          </Link>
+                        </div>
+                      </SheetTitle>
+                      <SheetDescription>
+                        <ul className="flex flex-col gap-10 mt-36 items-center ">
+                          {MenuLinks.map((data, i) => {
+                            const isActive = pathname === data.Link; // Compare pathname directly with Link
+                            return (
+                              <li key={i}>
+                                {/* Wrap Link with PageTransition */}
+
+                                <Link
+                                  href={data.Link}
+                                  className={
+                                    isActive
+                                      ? "text-[#009e66] text-2xl  nav-links relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#009e66] after:w-full  after:scale-x-100 after:transition after:duration-300 after:origin-center"
+                                      : "text-2xl nav-links relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#009e66] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
+                                  }
+                                >
+                                  {data.Name}
+                                </Link>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </SheetDescription>
+                    </SheetHeader>
+                  </SheetContent>
+                </Sheet>
               </i>
             </div>
-            <div></div>
           </div>
         </div>
       </div>
